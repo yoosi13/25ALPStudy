9317f0151efe7af0235625e3af1a8fe9b1a7c822
lxxzdrgnl <pung4905@naver.com>
2025-03-25 02:41:02 +0900

    upload

--- 2025-1/Intermediate/lxxzdrgnl/BinarySearch/practice/10815_NumberCard.cpp
+++ 2025-1/Intermediate/lxxzdrgnl/BinarySearch/practice/10815_NumberCard.cpp
@@ -10,10 +10,9 @@ int main() {
 
     int N;
     cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> arr[i];
-    }
+    for (int i = 0; i < N; i++) cin >> arr[i];
     sort(arr, arr + N);
+
     int M, t;
     cin >> M;
     for (int i = 0; i < M; i++) {
