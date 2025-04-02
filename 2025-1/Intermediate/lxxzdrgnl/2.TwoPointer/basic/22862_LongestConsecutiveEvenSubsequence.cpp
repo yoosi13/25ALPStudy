11207563c6a4f509546f96040e4cebeb978275be
lxxzdrgnl <pung4905@naver.com>
2025-04-02 15:01:09 +0900

    upload

--- 2025-1/Intermediate/lxxzdrgnl/2.TwoPointer/basic/22862_LongestConsecutiveEvenSubsequence.cpp
+++ 2025-1/Intermediate/lxxzdrgnl/2.TwoPointer/basic/22862_LongestConsecutiveEvenSubsequence.cpp
@@ -12,5 +12,20 @@ int main() {
     vector<int> a(N);
     for (int i = 0; i < N; i++) cin >> a[i];
 
-    
+    int en = 0;
+    int cnt = 0;
+    int len = 0;
+
+    for (int st = 0; st < N; st++) {
+        while (en < N && cnt + (a[en] % 2 != 0) <= K) {
+            if (a[en] % 2 != 0) cnt++;
+            en++;
+        }
+
+        len = max(len, en - st - cnt);
+
+        if (a[st] % 2 != 0) cnt--;
+    }
+
+    cout << len;
 }
