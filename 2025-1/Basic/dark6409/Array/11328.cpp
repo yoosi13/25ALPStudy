76b0e51787f3baf14cd17fd00b85f7f76bea91be
JUNE-E <36994762+JUNE-E@users.noreply.github.com>
2025-04-30 16:18:07 +0900

    Update 11328.cpp

--- 2025-1/Basic/dark6409/Array/11328.cpp
+++ 2025-1/Basic/dark6409/Array/11328.cpp
@@ -1,6 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n; cin >> n;
+    
+    for(int i=0; i<n; i++){
+        string s1, s2; cin >> s1 >> s2;
+        sort(s1.begin(), s1.end());
+        sort(s2.begin(), s2.end());
+        
+        if(s1 == s2)
+            cout << "Possible\n";
+        else
+            cout << "Impossible\n";
+    }
+
+    return 0;
+}
+
+
+
+
+
+// 이전 코드
+#include <bits/stdc++.h>
+using namespace std;
+
 string func1(string s1, string s2){
     int mlen = s1.length();
     
