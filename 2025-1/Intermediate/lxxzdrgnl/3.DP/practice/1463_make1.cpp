3a05252e0130c5ebb92cf6d8e64b485e7f91e12b
lxxzdrgnl <pung4905@naver.com>
2025-05-12 23:52:44 +0900

    upload

---
+++ 2025-1/Intermediate/lxxzdrgnl/3.DP/practice/1463_make1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+using namespace std;
+
+int d[1000005];
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    
+    int n;
+    cin >> n;
+
+    d[1] = 0;
+    for (int i = 2; i <= n; i++) {
+        d[i] = d[i - 1] + 1;
+        if (i % 2 == 0) d[i] = min(d[i], d[i / 2] + 1);
+        if (i % 3 == 0) d[i] = min(d[i], d[i / 3] + 1);
+    }
+    cout << d[n];
+}
