18aa837501c2a99cfde9debddc9defca23319b71
lxxzdrgnl <pung4905@naver.com>
2025-05-22 14:57:35 +0900

    upload

--- 2025-1/Intermediate/lxxzdrgnl/5.Divide and Conquer/presentation/1074_compression.cpp
+++ 2025-1/Intermediate/lxxzdrgnl/5.Divide and Conquer/presentation/1074_compression.cpp
@@ -3,21 +3,23 @@
 using namespace std;
 
 int visit(int N, int r, int c) {
+    if (N == 0) return 0;
+
     int half = 1 << (N - 1);
     int area = half * half;
 
     if (r < half && c < half) {
         // 1사분면
-        return visit(n - 1, r, c);
+        return visit(N - 1, r, c);
     } else if (r < half && c >= half) {
         // 2사분면
-        return area + visit(n - 1, r, c - half);
+        return area + visit(N - 1, r, c - half);
     } else if (r >= half && c < half) {
         // 3사분면
-        return 2 * area + visit(n - 1, r - half, c);
+        return 2 * area + visit(N - 1, r - half, c);
     } else {
         // 4사분면
-        return 3 * area + visit(n - 1, r - half, c - half);
+        return 3 * area + visit(N - 1, r - half, c - half);
     }
 }
 
